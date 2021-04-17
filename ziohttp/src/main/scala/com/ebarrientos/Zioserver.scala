3c6496062f9b7d7c89727833976c31cefd06dd65
Eduardo Barrientos <doomsday@pop-os.localdomain>
2021-04-17 09:26:14 -0400

    Make the server stoppable

--- ziohttp/src/main/scala/com/ebarrientos/Zioserver.scala
+++ ziohttp/src/main/scala/com/ebarrientos/Zioserver.scala
@@ -3,15 +3,27 @@ package com.ebarrientos
 import zio._
 import zhttp.http._
 import zhttp.service.Server
+import zio.console._
 
 object Zioserver extends zio.App {
 
-  val dataroute = Http.collect {
-    case Method.GET -> Root / "data" / id =>
-      Response.text("texto")
+  val dataroute = Http.collect { case Method.GET -> Root / "data" / id =>
+    Response.text(s"Texto: $id")
   }
 
-  override def run(args: List[String]): URIO[ZEnv,ExitCode] =
-    Server.start(9000, dataroute).exitCode
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] =
+    Server
+      .start(9000, dataroute)
+      .raceFirst(shutdownTask)
+      .exitCode
 
+  // val shutdownTask = getStrLn.catchAll(_ => Task.effectTotal(""))
+  val shutdownTask =
+    (for {
+      _ <- getStrLn
+      _ <- getStrLn  // Need a second one because the first one goes right on by
+      _ <- putStrLn("Shutting down server")
+    } yield "OK!")
+      .catchAll(e => Task.effectTotal(e.getMessage()))
+      .flatMap(t => putStrLn(s"Result: $t"))
 }
