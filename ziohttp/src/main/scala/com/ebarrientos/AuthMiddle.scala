c40f402dc1a86b325f590d7b382054152dcdeea6
Eduardo Barrientos <doomsday@pop-os.localdomain>
2021-04-19 09:14:55 -0400

    Adjust middleware variance

--- ziohttp/src/main/scala/com/ebarrientos/AuthMiddle.scala
+++ ziohttp/src/main/scala/com/ebarrientos/AuthMiddle.scala
@@ -8,6 +8,6 @@ case class Wrapped[+A](req: Request, data: A)
 /** Middleware to extract info from a request
   * If the info cannot be extracted, finishes with an error E
   */
-trait Middleware[E, A] {
+trait Middleware[+E, +A] {
   def apply(r: Request): IO[E, Wrapped[A]]
 }
